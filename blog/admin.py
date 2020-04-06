dd6ed2cc005eecda0a2a66b0d22374c23007cc05
vemretellier@gmail.com <vemretellier@gmail.com>
2020-04-07 02:24:44 +0300

    video3-admin panel video4-category blog iliskisi video5-resim image upload

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import Category, Blog
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'status']
+    list_filter = ['status', 'category']
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Blog,BlogAdmin)
