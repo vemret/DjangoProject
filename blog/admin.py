fb94aa994ccbc04e3e641c021669be6695ff9c18
vemretellier@gmail.com <vemretellier@gmail.com>
2020-04-12 01:52:29 +0300

    video7-admin resim galerisi video8-resimlerin gösterilmesi video9-setting ayarlar

--- blog/admin.py
+++ blog/admin.py
@@ -1,16 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
-from blog.models import Category, Blog
+from blog.models import Category, Blog, Images
 
+class BlogImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status']
+    list_display = ['title','status', 'image_tag']
     list_filter = ['status']
+    readonly_fields = ('image_tag',)
+
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'status']
+    list_display = ['title', 'category', 'status', 'image_tag']
+    readonly_fields = ('image_tag',)
     list_filter = ['status', 'category']
+    inlines = [BlogImageInline]
+
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'blog', 'image_tag']
+    readonly_fields = ('image_tag',)
+
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Blog,BlogAdmin)
+admin.site.register(Images,ImagesAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.safestring import mark_safe
+
+
 class Category(models.Model):
     STATUS = (
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=150)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -19,13 +22,19 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+
+
 class Blog(models.Model):
     STATUS = (
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
     category = models.ForeignKey(Category, on_delete=models.CASCADE) #relation with Category taable
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=150)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -36,3 +45,21 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+
+
+class Images(models.Model):
+    blog=models.ForeignKey(Blog,on_delete=models.CASCADE)
+    title = models.CharField(max_length=150,blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
