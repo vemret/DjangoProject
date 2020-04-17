df08668bc5e7b1e7a9529596a275d41c3234aa63
vemretellier@gmail.com <vemretellier@gmail.com>
2020-04-18 00:49:54 +0300

    video12

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
 # Create your models here.
@@ -23,8 +24,8 @@ class Setting(models.Model):
     facebook = models.CharField(blank=True, max_length=50)
     instagram = models.CharField(blank=True, max_length=50)
     twitter = models.CharField(blank=True, max_length=50)
-    about = models.TextField(blank=True,)
-    contact = models.TextField(blank=True,)
+    about = RichTextUploadingField(blank=True,)
+    contact = RichTextUploadingField(blank=True,)
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
