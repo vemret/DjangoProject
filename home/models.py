02b0a8e058751f83d24884651ef5d334d9be504e
vemretellier@gmail.com <vemretellier@gmail.com>
2020-05-05 22:35:56 +0300

    vide 14

--- home/models.py
+++ home/models.py
@@ -2,6 +2,8 @@ from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
 # Create your models here.
+from django.forms import ModelForm, TextInput, Textarea
+
 
 class Setting(models.Model):
     STATUS = (
@@ -11,6 +13,7 @@ class Setting(models.Model):
     title = models.CharField(max_length=150)
     keywords = models.CharField(blank=True, max_length=255)
     description = models.CharField(max_length=255)
+    company = models.CharField(blank=True, max_length=50)
     bloggername = models.CharField(max_length=50)
     address = models.CharField(blank=True, max_length=150)
     phone = models.CharField(blank=True, max_length=15)
@@ -26,9 +29,43 @@ class Setting(models.Model):
     twitter = models.CharField(blank=True, max_length=50)
     about = RichTextUploadingField(blank=True,)
     contact = RichTextUploadingField(blank=True,)
+    references = RichTextUploadingField(blank=True, )
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+
+class ContactFormMessage(models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('Read', 'Read'),
+        ('Closed', 'Closed'),
+    )
+
+    name = models.CharField(blank=True, max_length=20)
+    email = models.CharField(blank=True, max_length=50)
+    subject = models.CharField(blank=True, max_length=50)
+    message = models.CharField(blank=True, max_length=250)
+    status = models.CharField(max_length=10, choices=STATUS, default='New')
+    ip = models.CharField(blank=True, max_length=20)
+    note = models.CharField(blank=True, max_length=100)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class ContactFormm(ModelForm):
+    class Meta:
+        model = ContactFormMessage
+        fields = ['name', 'email', 'subject', 'message']
+        widgets = {
+            'name': TextInput(attrs={'class': 'wpcf7-form-control', 'placeholder': 'Name & Surname'}),
+            'subject': TextInput(attrs={'class': 'wpcf7-form-control', 'placeholder': 'Subject'}),
+            'email': TextInput(attrs={'class': 'wpcf7-form-control', 'placeholder': 'Email Address'}),
+            'message': Textarea(attrs={'class': 'wpcf7-form-control', 'placeholder': 'Your Message', 'rows': 'S'}),
+        }
--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormMessage, ContactFormm
 
 
 def index(request):
@@ -21,6 +22,21 @@ def referanslar(request):
     return render(request, 'referanslar.html', context)
 
 def iletisim(request):
+
+    if request.method == 'POST': #Form Post eddildi ise
+        form = ContactFormm(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage() #model ile baglantı kur
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save() # veri tabanı kayıt
+            messages.success(request, "Mesajınız başarı ile gönderildi")
+            return HttpResponseRedirect('/iletisim')
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting, }
+    form = ContactFormm()
+    context = {'setting': setting, 'form': form}
     return render(request, 'iletisim.html', context)
